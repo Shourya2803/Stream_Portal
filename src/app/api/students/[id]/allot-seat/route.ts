9f04c275188dff4bf04e2e2bda286e87876fccb1
tprachi09082005@gmail.com <mittalshourya2803@gmail.com>
2025-07-23 14:00:52 +0530

    Stream Portal

--- src/app/api/students/[id]/allot-seat/route.ts
+++ src/app/api/students/[id]/allot-seat/route.ts
@@ -4,9 +4,9 @@ import prisma from "@/lib/prisma";
 
 export async function POST(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
   const body = await req.json();
   const seat = body.seat;
 
